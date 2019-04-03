20435bd76de5b629d3283298d434045da915d926
bread_moon <moonjinh123@naver.com>
2019-04-03 23:01:29 +0900

    loading frame

---
+++ app/src/main/java/com/example/eyelike/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.eyelike;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VisionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
