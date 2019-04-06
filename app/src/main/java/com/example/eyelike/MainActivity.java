02c587db9ccc97434dcbfda6eb337d3f42069059
bread_moon <moonjinh123@naver.com>
2019-04-06 13:38:37 +0900

    camera

--- app/src/main/java/com/example/eyelike/MainActivity.java
+++ app/src/main/java/com/example/eyelike/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.eyelike;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, VisionActivity.class);
+                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
                 startActivity(intent);
             }
         });
